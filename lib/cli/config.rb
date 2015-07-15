7e34ab6127530de0e0c967ba94541f8ac55b3a4b
Kenny Debrauwer <kenny@axsh.net>
2015-07-15 14:26:27 +0900

    create new file when missing

--- lib/cli/config.rb
+++ lib/cli/config.rb
@@ -7,11 +7,16 @@ module DucttapeCLI
   
   class Config < Thor
     
-    desc "database <name>", "change the atabase being used"
+    desc "database <name>", "change the database being used (creates new file when file does not exist yet)"
     def database(name)
       config = Config.loadConfig()        
       config[:database] = name
       Config.writeConfig(config)
+      
+      if (!File.file?(name))
+        File.open("#{name}.yml", 'w') {|f| f.write("---") }
+      end
+      
       puts config.to_yaml()
     end
     
