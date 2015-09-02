17c0501bd8ca5e1b5612378532e9b651391f661e
Kenny Debrauwer <kenny@axsh.net>
2015-09-02 18:34:27 +0900

    fix naming

--- test/setup.sh
+++ test/setup.sh
@@ -5,18 +5,18 @@ set -o pipefail
 set -x
 
 # jenkins defines "WORKSPACE"
-cd "${WORKSPACE:-"/home/vagrant/ducttape"}"
+cd "${WORKSPACE:-"/home/vagrant/netjoin"}"
 bundle install
 
-echo "#### Initializing ducttape"
+echo "#### Initializing netjoin"
 rm -f database.yml
-bin/ducttape init
+bin/netjoin init
 
 echo "#### Adding Server"
-bin/ducttape servers linux add server1 --ip-address 192.168.56.10 --username vagrant --key-pem test/keys/insecure_private_key --file-ca-crt test/keys/ca.crt --file-conf test/keys/server.conf --file-crt test/keys/server.crt --file-key test/keys/server.key --file-pem test/keys/dh2048.pem
+bin/netjoin servers linux add server1 --ip-address 192.168.56.10 --username vagrant --key-pem test/keys/insecure_private_key --file-ca-crt test/keys/ca.crt --file-conf test/keys/server.conf --file-crt test/keys/server.crt --file-key test/keys/server.key --file-pem test/keys/dh2048.pem
 
 echo "#### Adding Client"
-bin/ducttape clients linux add client1 --server server1 --ip-address 192.168.56.100 --username vagrant --key-pem test/keys/insecure_private_key --file_key test/keys/client1.ovpn
+bin/netjoin clients linux add client1 --server server1 --ip-address 192.168.56.100 --username vagrant --key-pem test/keys/insecure_private_key --file_key test/keys/client1.ovpn
 
 echo "#### Installing Server"
 bin/netjoin servers linux install server1; echo $?
