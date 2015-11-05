c8bcdbb79f2da2b876313109803297d556700631
akry <a-yokokawa@axsh.net>
2015-11-05 17:52:24 +0900

    Add helper methods for logger

--- lib/netjoin/helpers/logger.rb
+++ lib/netjoin/helpers/logger.rb
@@ -6,6 +6,22 @@ module Netjoin
       def logger
         Netjoin.logger
       end
+
+      def info(msg)
+        logger.info msg
+      end
+
+      def error(msg)
+        logger.error msg
+      end
+
+      def debug(msg)
+        logger.debug msg
+      end
+
+      def warn(msg)
+        logger.warn msg
+      end
     end
   end
 end
