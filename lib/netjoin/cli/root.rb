3ac2b4100784d6b71c1f73c2e5515ebc2218bf3b
akry <a-yokokawa@axsh.net>
2015-11-05 17:52:44 +0900

    Refactor init command

--- lib/netjoin/cli/root.rb
+++ lib/netjoin/cli/root.rb
@@ -1,66 +1,66 @@
+# -*- coding: utf-8 -*-
+
+require 'fileutils'
+
 module Netjoin::Cli
 
   class Root < Thor
-
-    desc "config SUBCOMMAND ...ARGS", "manage configuration"
-    subcommand "config", Netjoin::Cli::Config
-
-    desc "clients SUBCOMMAND ...ARGS", "manage clients"
-    subcommand "clients", Netjoin::Cli::Clients
-
-    desc "servers SUBCOMMAND ...ARGS", "manage servers"
-    subcommand "servers", Netjoin::Cli::Servers
+    include Netjoin::Helpers::Logger
 
     desc "init", "init netjoin"
     def init()
-      if (!File.file?('config.yml'))
-        dist_config = Root.load_file('config-dist.yml')
-        Root.write_file('config.yml', dist_config.to_yaml)
-        puts "Configuration file 'config.yml' created!"
-      else
-        puts "Configuration file 'config.yml' already exists, skipping!"
-      end
-      if(!File.file?('database.yml'))
-        dist_database = Root.load_file('database-dist.yml')
-        Root.write_file('database.yml', dist_database.to_yaml)
-        puts "Database file 'database.yml' created!"
-      else
-        puts "Database file 'database.yml' already exists, skipping!"
+      ['config', 'database'].each do |name|
+        if File.exist?("#{name}.yml")
+          info "#{name}.yml exists"
+        else
+          FileUtils.cp("#{name}-dist.yml", "#{name}.yml")
+          info "create #{name}.yml"
+        end
       end
     end
 
-    desc "export","Export database"
-    def export()
-      # Read database file
-      database = Root.load_database()
-      puts database.to_yaml()
-    end
+    # desc "config SUBCOMMAND ...ARGS", "manage configuration"
+    # subcommand "config", Netjoin::Cli::Config
 
-    def self.get_from_config(name)
-      config = Root.load_file('config.yml')
-      return database = config[name]
-    end
+    # desc "clients SUBCOMMAND ...ARGS", "manage clients"
+    # subcommand "clients", Netjoin::Cli::Clients
 
-    def self.load_database()
-      return Root.load_file("#{Root.get_from_config(:database)}.yml")
-    end
+    # desc "servers SUBCOMMAND ...ARGS", "manage servers"
+    # subcommand "servers", Netjoin::Cli::Servers
 
-    def self.write_database(database)
-      return Root.write_file("#{Root.get_from_config(:database)}.yml", database.to_yaml)
-    end
 
-    def self.load_file(name)
-      file = YAML.load_file(name)
-      if(!file)
-        file = {}
-      end
-      return file
-    end
+    # desc "export","Export database"
+    # def export()
+    #   # Read database file
+    #   database = Root.load_database()
+    #   puts database.to_yaml()
+    # end
 
-    def self.write_file(name, data)
-      File.open(name,'w') do |h|
-        h.write data
-      end
-    end
+    # def self.get_from_config(name)
+    #   config = Root.load_file('config.yml')
+    #   return database = config[name]
+    # end
+
+    # def self.load_database()
+    #   return Root.load_file("#{Root.get_from_config(:database)}.yml")
+    # end
+
+    # def self.write_database(database)
+    #   return Root.write_file("#{Root.get_from_config(:database)}.yml", database.to_yaml)
+    # end
+
+    # def self.load_file(name)
+    #   file = YAML.load_file(name)
+    #   if(!file)
+    #     file = {}
+    #   end
+    #   return file
+    # end
+
+    # def self.write_file(name, data)
+    #   File.open(name,'w') do |h|
+    #     h.write data
+    #   end
+    # end
   end
 end
