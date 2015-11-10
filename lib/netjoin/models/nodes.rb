702b52cac861662559f72c2b733818c8627d68e8
akry <a-yokokawa@axsh.net>
2015-11-10 13:36:39 +0900

    Provision kvm on bare-metal

--- lib/netjoin/models/nodes.rb
+++ lib/netjoin/models/nodes.rb
@@ -1,16 +1,33 @@
 # -*- coding: utf-8 -*-
 
 require 'bcrypt'
+require 'net/ssh'
+require 'net/scp'
 
 module Netjoin::Models
   class Nodes < Base
     include Netjoin::Helpers::Logger
 
     def self.validate(options)
-      encrypt_password(options)
+      #encrypt_password(options)
     end
 
     def create
+      if self.ssh_from
+        ip = self.ssh_from['ssh_ip_address']
+        user = self.ssh_from['ssh_user']
+        password = self.ssh_from['ssh_password']
+        fp = "#{Netjoin::ROOT}/misc/run.sh"
+        Net::SCP.upload!(ip, user, fp, "/root", :ssh => {:password => password})
+        Net::SSH.start(ip, user, :password => password) do |ssh|
+          if self.provision
+            info ssh.exec!("yum -y install git parted kpartx bridge-utils || :")
+            info ssh.exec!("git clone https://github.com/hansode/vmbuilder.git || :")
+            info ssh.exec!("cd vmbuilder/kvm/rhel/6/; ./vmbuilder.sh --ip=10.100.0.10 --mask=255.255.255.0; mv centos-6.7_x86_64.raw /root")
+            info ssh.exec!("chmod +x ./run.sh; ./run.sh #{self.name}")
+          end
+        end
+      end
     end
 
     private
@@ -21,6 +38,7 @@ module Netjoin::Models
         ssh_from = node['ssh_from']
         node['ssh_from'] = hash['nodes'][ssh_from]
       end
+      node['name'] = params[:name]
       node
     end
 
