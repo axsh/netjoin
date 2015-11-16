6c09197e35e16b7419c1955001e0d57119777be7
akry <a-yokokawa@axsh.net>
2015-11-16 17:24:56 +0900

    Invoke install method on driver module

--- lib/netjoin/models/networks.rb
+++ lib/netjoin/models/networks.rb
@@ -4,13 +4,24 @@ module Netjoin::Models
   class Networks < Base
     include Netjoin::Helpers::Logger
 
+    def initialize(params)
+      super(params)
+    end
+
     def self.validate(options)
+      true
     end
 
     def create
+      d = Netjoin::Drivers.const_get(self.driver.capitalize)
+      self.nodes.each do |node|
+        n = Netjoin::Models::Nodes.new(name: node)
+        d.install(n, self)
+      end
     end
 
     def shape(hash, params)
+      hash['networks'][params[:name]]
     end
   end
 end
