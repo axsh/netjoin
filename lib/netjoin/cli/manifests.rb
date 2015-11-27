f080914c2cc280696f11966040408903640f77fd
akry <a-yokokawa@axsh.net>
2015-11-27 18:37:16 +0900

    Pass option hash to manifests model.

--- lib/netjoin/cli/manifests.rb
+++ lib/netjoin/cli/manifests.rb
@@ -14,6 +14,7 @@ module Netjoin::Cli
 
     def add(name)
       info "add #{name}"
+      Netjoin::Models::Manifests.add(name, options.to_h)
     end
   end
 end
--- lib/netjoin/models/manifests.rb
+++ lib/netjoin/models/manifests.rb
@@ -3,5 +3,15 @@
 module Netjoin::Models
   class Manifests < Base
     include Netjoin::Helpers::Logger
+
+    def self.validate(options)
+      true
+    end
+
+    private
+
+    def shape(hash, params)
+      hash['manifests'][params[:name]]
+    end
   end
 end
