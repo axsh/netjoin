824362490f580e2e9ce66625200fdbed722c8957
akry <a-yokokawa@axsh.net>
2015-12-02 17:53:20 +0900

    Add some lookup methods.

--- lib/netjoin/models/topologies.rb
+++ lib/netjoin/models/topologies.rb
@@ -8,6 +8,18 @@ module Netjoin::Models
       true
     end
 
+    def self.get_all_server_nodes
+      server_nodes = []
+      Netjoin.db['topologies'].each do |k,v|
+        v['server_nodes'].each { |b| server_nodes << b }
+      end
+      server_nodes.uniq
+    end
+
+    def self.get_all_server_nodes_except(name)
+      get_all_server_nodes.select { |n| n != name }
+    end
+
     private
 
     def shape(hash, params)
