cab18deab1ee64cee225aaa89cf00fa28de65787
akry <a-yokokawa@axsh.net>
2016-03-10 19:06:45 +0900

    Load gems from 'bundler/setup.rb' thus --standalone option is necessary when bundle install

---
+++ lib/netjoin/rubygems.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+setup_rb = File.expand_path("../../../vendor/bundle/bundler/setup.rb", __FILE__)
+
+if File.exist?(setup_rb)
+  require setup_rb
+else
+  abort "[ERROR]: netjoin requires 'bundle install' with '--standalone' option."
+end
