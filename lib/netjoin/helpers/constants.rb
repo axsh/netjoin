17bd11ba3a869c0d5cc843aece3191f8c301a4df
akry <a-yokokawa@axsh.net>
2016-03-23 16:08:33 +0900

    Rename builder file

--- lib/netjoin/helpers/constants.rb
+++ lib/netjoin/helpers/constants.rb
@@ -4,8 +4,8 @@ require 'yaml'
 
 module Netjoin::Helpers
   module Constants
-    DATABASE_YAML = 'database.yml'
-    CONFIG_YAML = 'config.yml'
+    DATABASE_YAML = 'netjoin.yml'
+    CONFIG_YAML = 'netjoin_config.yml'
 
     GLOBAL_CIDR = ENV['GLOBAL_CIDR']
   end
