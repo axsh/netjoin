e95eb88ad238359293847037c620c424bec2d054
akry <a-yokokawa@axsh.net>
2016-03-23 16:14:08 +0900

    Implement methods: show_ip, setup_tunnel

--- lib/netjoin/cli/root.rb
+++ lib/netjoin/cli/root.rb
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 require 'yaml'
+require 'net/ssh'
+require 'net/ssh/proxy/command'
 
 module Netjoin::Cli
 
@@ -26,19 +28,60 @@ module Netjoin::Cli
 
     desc "up", "provision resources"
     def up
-      Netjoin.db = YAML.load_file(DATABASE_YAML).symbolize_keys
-      Netjoin.config = YAML.load_file(CONFIG_YAML).symbolize_keys
-
       list_to_provision(db).each do |node_name|
         type = db[:nodes][node_name][:provision][:spec][:type].capitalize
         Netjoin::Drivers.const_get(type).create(node_name)
       end
     end
 
+    desc "show_ip", "show ip address for specific type"
+    def show_ip(type)
+      ips = []
+      db[:nodes].each do |key, value|
+        if value.include?(:provision) && value[:provision][:spec][:type] == type
+          value[:provision][:spec][:nics].each do |k, v|
+            ips << v[:ipaddr]
+          end
+        end
+      end
+      p ips
+    end
+
+    desc "setup_tunnel", "setup_tunnel"
+    def setup_tunnel(type, ip)
+      node = db[:nodes].select {|k,v| v.include?(:provision) && v[:provision][:spec][:type] == type }.first.last
+
+      ssh_options = {}
+
+      if node[:ssh].include?(:from)
+        parent = db[:nodes][node[:ssh][:from].to_sym]
+
+        proxy = Net::SSH::Proxy::Command.new("
+          ssh #{parent[:ssh][:ip]} \
+            -l #{parent[:ssh][:user]} \
+            -o StrictHostKeyChecking=no \
+            -o UserKnownHostsFile=/dev/null \
+            -W %h:%p -i #{parent[:ssh][:key]}
+        ")
+
+        ssh_options[:proxy] = proxy
+      end
+
+      ssh_options[:keys] = [node[:ssh][:key]]
+      ssh_options[:user_known_hosts_file] = "/dev/null"
+      ssh_options[:paranoid] = false
+
+      Net::SSH.start(node[:ssh][:ip], node[:ssh][:user], ssh_options) do |ssh|
+        ssh_exec(ssh, [
+          "tun=`cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 4 | head -n 1`; sudo ovs-vsctl add-port brtun g${tun} -- set interface g${tun} type=gre options:remote_ip=#{ip}"
+        ])
+      end
+    end
+
     no_tasks {
       def list_to_provision(db)
         db[:nodes].inject([]) do |nodes_to_provision, (key, value)|
-          if value.include?(:provision)
+          if value.include?(:provision) && (!value[:provision].include?(:provisioned) || value[:provision][:provisioned] == false)
             nodes_to_provision << key
           end
           nodes_to_provision
